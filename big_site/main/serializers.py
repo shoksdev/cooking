de7210570b4206bcaa76d5a5186c0fa82aa1905d
Atlanta19 <shorty44.business@gmail.com>
2023-10-17 15:14:18 +0700

    Change Dishes serializer

--- big_site/main/serializers.py
+++ big_site/main/serializers.py
@@ -1,9 +1,24 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from .models import Dishes
 
 
-class DishesSerializer(ModelSerializer):
+class DishesSerializer(serializers.ModelSerializer):
+    new_ingredients = serializers.SerializerMethodField(method_name='get_new_ingredients')
+
     class Meta:
         model = Dishes
         fields = '__all__'
+
+    def get_new_ingredients(self, dish_item=Dishes):
+        ingredients = dish_item.ingredients
+        ingredients_dict = {}
+        ingredients_dict_new = {}
+        for name in ingredients.split('\n'):
+            key, *value = name.split('-')
+            ingredients_dict[key] = value
+        for key, value in ingredients_dict.items():
+            new_key, new_value = key.strip(), value[0].strip()
+            ingredients_dict_new[new_key] = new_value
+
+        return ingredients_dict_new
--- big_site/main/urls.py
+++ big_site/main/urls.py
@@ -6,7 +6,6 @@ from .views import DishesViewSet, CreateDishView
 router = routers.SimpleRouter()
 router.register(r'dishes', DishesViewSet)
 
-
 urlpatterns = [
     path('', include(router.urls)),
     path('dishes/create', CreateDishView.as_view(), name='create_dish')
